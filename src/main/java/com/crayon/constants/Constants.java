e5e4f49a7b32bfac62428431ecba0aa6b1e51063
SaravanaManikandan92 <saravanamanikandanmuthu@gmail.com>
2025-12-20 19:27:48 +0530

    Update Subscription changes

--- src/main/java/com/crayon/constants/Constants.java
+++ src/main/java/com/crayon/constants/Constants.java
@@ -6,18 +6,22 @@ public class Constants {
 
     //
 
-    public static List<String> clientList = Arrays.asList("57747f5d-6758-4afa-9d65-0a3097b68389","dd636335-efbe-491b-b3b3-d8225c8dc1d1","6434afd2-811c-400c-ac13-d133b476086a","30b1b92c-cd3c-46c2-8375-de13cb5d20ae","1d742172-6ea3-46d0-b83e-9553c5879414");
+    public static List<String> clientList = Arrays.asList("57747f5d-6758-4afa-9d65-0a3097b68389","dd636335-efbe-491b-b3b3-d8225c8dc1d1",
+            "6434afd2-811c-400c-ac13-d133b476086a","30b1b92c-cd3c-46c2-8375-de13cb5d20ae","1d742172-6ea3-46d0-b83e-9553c5879414",
+            "36ef3b68-aae4-4521-b367-8cefeff85b29");
   //  public static List<String> clientList = Arrays.asList("Magically_Genius","Outbox_Labs","MicroSaaS_Labs","Cheap_Inbox");
   public static Map<String,String> clientListMap = Map.ofEntries(
           Map.entry("57747f5d-6758-4afa-9d65-0a3097b68389", "Magically_Genius"),
           Map.entry("dd636335-efbe-491b-b3b3-d8225c8dc1d1", "Outbox_Labs"), // default February
           Map.entry("6434afd2-811c-400c-ac13-d133b476086a", "MicroSaaS_Labs"),
           Map.entry("30b1b92c-cd3c-46c2-8375-de13cb5d20ae", "Cheap_Inbox"),
-          Map.entry("1d742172-6ea3-46d0-b83e-9553c5879414","axalin"));
-    //public static String INFLUX_TOKEN ="Kq7seBQiRWyqbkF4DYe5mH-6FkVoyW6xTXF5LzGsBihqTpkULkXBfLhYhtyyBA9tdgTN3SPrDeitO6on-_cEdA==";//"Token A7MJiqEvG-3B-n5geDnVWQ_61KnpWbLpueTPywlr7PrsX19YTqXyikVuTnjnshDOEVv9-1wUa7XSMbSnxjm0Tw==";
-    public static String INFLUX_TOKEN ="my-super-token";//;"Kq7seBQiRWyqbkF4DYe5mH-6FkVoyW6xTXF5LzGsBihqTpkULkXBfLhYhtyyBA9tdgTN3SPrDeitO6on-_cEdA=="; //
-    //public static String INFLUX_ORG ="SaraOrg";//"f086a9856ff86826" // //AxelOrg
-    public static String INFLUX_ORG ="AxelOrg";
+          Map.entry("1d742172-6ea3-46d0-b83e-9553c5879414","axalin"),
+          Map.entry("36ef3b68-aae4-4521-b367-8cefeff85b29","Receptive_Marketing")
+  );
+    public static String INFLUX_TOKEN ="Kq7seBQiRWyqbkF4DYe5mH-6FkVoyW6xTXF5LzGsBihqTpkULkXBfLhYhtyyBA9tdgTN3SPrDeitO6on-_cEdA==";//"Token A7MJiqEvG-3B-n5geDnVWQ_61KnpWbLpueTPywlr7PrsX19YTqXyikVuTnjnshDOEVv9-1wUa7XSMbSnxjm0Tw==";
+   // public static String INFLUX_TOKEN ="my-super-token";//;"Kq7seBQiRWyqbkF4DYe5mH-6FkVoyW6xTXF5LzGsBihqTpkULkXBfLhYhtyyBA9tdgTN3SPrDeitO6on-_cEdA=="; //
+    public static String INFLUX_ORG ="SaraOrg";//"f086a9856ff86826" // //AxelOrg //"SaraOrg"
+   // public static String INFLUX_ORG ="AxelOrg";
     public static String INFLUX_BUCKET ="CrayonAudit";
     public static String INFLUX_URL ="http://localhost:8086";
     public static String INFLUX_URL_QUERY ="http://localhost:8086/api/v2/query";
@@ -30,6 +34,7 @@ public class Constants {
     public static int MicroSaaS_Labs_INVOICE_ID=211425;
     public static int Cheap_Inbox_INVOICE_ID=212451;
     public static int Axalin_INVOICE_ID=203480;
+    public static int  Receptive_Marketing=213628;
 
     public static int cancelSubscription=4;
 
@@ -38,7 +43,9 @@ public class Constants {
             Map.entry("dd636335-efbe-491b-b3b3-d8225c8dc1d1", "180723"), // default February
             Map.entry("6434afd2-811c-400c-ac13-d133b476086a", "211425"),
             Map.entry("30b1b92c-cd3c-46c2-8375-de13cb5d20ae", "212451"),
-            Map.entry("1d742172-6ea3-46d0-b83e-9553c5879414","203480"));//203480
+            Map.entry("1d742172-6ea3-46d0-b83e-9553c5879414","203480"),
+            Map.entry("36ef3b68-aae4-4521-b367-8cefeff85b29","213628")
+    );//203480
 
     public static final Map<Integer, Integer> baseMonthDateMap = Map.ofEntries(
             Map.entry(1, 31),
